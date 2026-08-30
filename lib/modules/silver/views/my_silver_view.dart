a2ebe0eba0b9f8fb004ae073f4503b6dfe76e1b1
Ashahad Shaikh <ashahadshaikh1002@gmail.com>
2026-08-30 16:27:58 +0530

    feat: integrate Firebase, geolocator, local notifications, and other plugins while adding new UI module and security certificates

--- lib/modules/silver/views/my_silver_view.dart
+++ lib/modules/silver/views/my_silver_view.dart
@@ -110,7 +110,6 @@ class _MySilverViewState extends State<MySilverView>
             IconButton(
               icon: Icon(Icons.notifications_outlined, color: ts),
               onPressed: () => Get.toNamed(AppRoutes.notifications),
-              onPressed: () {},
             ),
           ],
         ),
